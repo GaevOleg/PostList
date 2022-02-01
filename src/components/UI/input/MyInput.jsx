af8a338d6764e544808b3a69a9c9a48b7bd67f28
RazorChrome <ogaev1997@gmail.com>
2022-02-01 14:58:44 +0700

    Removing files from gh-pages

--- src/components/UI/input/MyInput.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classes from './MyInput.module.css'
-
-export default function MyInput(props) {
-    return (
-       <input className={classes.myInput} {...props}/>
-    )
-}
